000d924e9ff13e466dda7af3844972257a0a05b1
Daniel Molnar <daniel4molnar@gmail.com>
2023-03-24 16:56:38 +0100

    Make function more generic

--- lib/instacamp/date_time_helper.ex
+++ lib/instacamp/date_time_helper.ex
@@ -4,9 +4,7 @@ defmodule Instacamp.DateTimeHelper do
   """
 
   @spec format_post_date(Date.t() | DateTime.t() | NaiveDateTime.t()) :: String.t()
-  def format_post_date(datetime) do
-    format = "%b %-d, %Y"
-
+  def format_post_date(datetime, format \\ "%b %-d, %Y") do
     case Timex.format(datetime, format, :strftime) do
       {:ok, timex_string} ->
         timex_string
