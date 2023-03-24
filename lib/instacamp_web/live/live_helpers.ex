553388b23ce7b6d1c8adb8c18e93f99e5ae5626b
Daniel Molnar <daniel4molnar@gmail.com>
2023-03-24 17:29:35 +0100

    Refactor live helpers

--- lib/instacamp_web/live/live_helpers.ex
+++ lib/instacamp_web/live/live_helpers.ex
@@ -11,12 +11,24 @@ defmodule InstacampWeb.LiveHelpers do
   @type tag :: Phoenix.HTML.Tag
 
   @doc """
-  Generates tag for menu link.
+  Generates sidebar tags.
   """
-  @spec sidebar_link_tag(String.t(), String.t(), String.t()) ::
-          {:safe, [binary | maybe_improper_list(any, binary | [])]}
+  @spec sidebar_link_tag(map()) ::
+          Phoenix.LiveView.Rendered.t()
+          | {:safe, [binary | maybe_improper_list(any, binary | [])]}
   def sidebar_link_tag(title, current_uri_path, menu_link) do
     content_tag(:li, title, class: "m-1 p-4 #{selected_link?(current_uri_path, menu_link)}")
+  end
+
+  def sidebar_link_tag(
+        %{title: title, current_uri_path: current_uri_path, menu_link: menu_link} = assigns
+      ) do
+    ~H"""
+    <%= content_tag :li, class: "flex items-center justify-between m-1 p-4 #{selected_link?(current_uri_path, menu_link)}" do %>
+      <span class="mr-2"><%= title %></span>
+      <%= render_slot(@inner_block) %>
+    <% end %>
+    """
   end
 
   @spec nav_link(map()) :: Phoenix.LiveView.Rendered.t()
@@ -39,11 +51,11 @@ defmodule InstacampWeb.LiveHelpers do
   end
 
   defp selected_link?(current_uri, menu_link) when current_uri == menu_link do
-    "rounded-md bg-indigo-100 dark:bg-slate-300 text-gray-900 ease-in-out"
+    "rounded-md bg-indigo-100 dark:bg-slate-400 text-gray-900 ease-in-out"
   end
 
   defp selected_link?(_current_uri, _menu_link) do
-    "rounded-md ease-in-out"
+    "rounded-md hover:bg-gray-100 dark:hover:bg-slate-500 ease-in-out"
   end
 
   @spec display_website_uri(String.t()) :: String.t()
