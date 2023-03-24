eca53223607269a649ef0ff5846a762f40a94c21
Daniel Molnar <daniel4molnar@gmail.com>
2023-03-24 17:41:38 +0100

    Redesign sidebar

--- lib/instacamp_web/live/settings_live/components/settings_sidebar_component.ex
+++ lib/instacamp_web/live/settings_live/components/settings_sidebar_component.ex
@@ -3,19 +3,32 @@ defmodule InstacampWeb.SettingsLive.Components.SettingsSidebarComponent do
 
   use InstacampWeb, :component
 
+  alias InstacampWeb.Components.Icons
+
   @spec settings_sidebar(map()) :: Phoenix.LiveView.Rendered.t()
   def settings_sidebar(assigns) do
     ~H"""
     <div class={@class}>
-      <ul class="flex flex-row justify-center md:flex-col">
-        <%= live_redirect(
-          sidebar_link_tag("Edit Profile", @current_uri_path, @settings_path),
-          to: @settings_path
-        ) %>
-        <%= live_redirect(
-          sidebar_link_tag("Change Password", @current_uri_path, @pass_settings_path),
-          to: @pass_settings_path
-        ) %>
+      <ul class="flex flex-row justify-center md:flex-col ">
+        <%= live_redirect to: @settings_path, replace: false do %>
+          <.sidebar_link_tag
+            title="Edit Profile"
+            current_uri_path={@current_uri_path}
+            menu_link={@settings_path}
+          >
+            <Icons.settings_icon />
+          </.sidebar_link_tag>
+        <% end %>
+
+        <%= live_redirect to: @pass_settings_path, replace: false do %>
+          <.sidebar_link_tag
+            title="Change Password"
+            current_uri_path={@current_uri_path}
+            menu_link={@pass_settings_path}
+          >
+            <Icons.confirm_icon />
+          </.sidebar_link_tag>
+        <% end %>
       </ul>
     </div>
     """
