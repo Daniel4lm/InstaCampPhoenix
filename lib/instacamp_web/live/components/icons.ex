5054466fc1bae57cbb084c46596e3d234a72c9b7
Daniel Molnar <daniel4molnar@gmail.com>
2023-03-25 14:22:17 +0100

    Add select arrow icon

--- lib/instacamp_web/live/components/icons.ex
+++ lib/instacamp_web/live/components/icons.ex
@@ -50,6 +50,22 @@ defmodule InstacampWeb.Components.Icons do
     """
   end
 
+  @spec select_arrow(assigns()) :: render()
+  def select_arrow(assigns) do
+    ~H"""
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="h-5 w-5"
+    >
+      <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+    </svg>
+    """
+  end
+
   @spec while_searching(assigns()) :: render()
   def while_searching(assigns) do
     ~H"""
