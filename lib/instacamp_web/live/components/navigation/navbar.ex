afa591df68d1638f40630b948da4fcbb7444012f
Daniel Molnar <daniel4molnar@gmail.com>
2023-03-25 14:09:37 +0100

    Update routes, fix style, remove theme switch

--- lib/instacamp_web/live/components/navigation/navbar.ex
+++ lib/instacamp_web/live/components/navigation/navbar.ex
@@ -15,12 +15,12 @@ defmodule InstacampWeb.Components.Navigation.Navbar do
   @spec app_navbar(assigns()) :: render()
   def app_navbar(assigns) do
     ~H"""
-    <header class="relative h-14 border-b dark:border-transparent flex fixed top-0 w-full bg-white dark:bg-slate-600 dark:text-text-slate-100 z-50">
+    <header class="h-14 border-b dark:border-transparent flex fixed top-0 w-full bg-white dark:bg-slate-600 dark:text-slate-100 z-50">
       <div class="flex justify-between md:justify-center items-center container mx-auto max-w-full md:w-11/12 2xl:w-8/12">
         <.nav_link
           active_path={@active_path}
           current_path="/"
-          link_path={Routes.live_path(@socket, InstacampWeb.HomeLive)}
+          link_path={Routes.feed_path(@socket, :index)}
         >
           <h1 class="text-xl md:text-2xl font-normal mx-4">InstaCamp</h1>
         </.nav_link>
@@ -30,7 +30,7 @@ defmodule InstacampWeb.Components.Navigation.Navbar do
         <nav class="lg:w-3/5">
           <ul class="flex items-center justify-end px-2 text-xs sm:text-sm md:text-base">
             <%= if @current_user do %>
-              <%= live_patch to: Routes.live_path(@socket, InstacampWeb.HomeLive), id: "app-home" do %>
+              <%= live_redirect to: Routes.feed_path(@socket, :index), id: "app-home" do %>
                 <li class="flex items-center justify-center text-gray-600 flex-col sm:flex-row dark:text-gray-200 hover:text-indigo-400">
                   <Icons.home_icon />
                   <span class="sm:ml-1 font-semibold">Home</span>
@@ -49,17 +49,6 @@ defmodule InstacampWeb.Components.Navigation.Navbar do
                 module={NotificationsComponent}
                 current_user={@current_user}
               />
-
-              <li
-                id="theme-switch"
-                class="relative ml-4 text-gray-600 dark:text-gray-200 hover:text-indigo-400"
-                phx-hook="ToolTip"
-              >
-                <Switch.toggle />
-                <span class="bottom-tooltip-text px-4 py-2 rounded-md bg-indigo-500 dark:bg-slate-500 text-white text-sm ">
-                  Day/Night Theme
-                </span>
-              </li>
 
               <SettingsMenu.menu
                 current_user={@current_user}
@@ -81,14 +70,14 @@ defmodule InstacampWeb.Components.Navigation.Navbar do
                 <%= live_redirect("Log In",
                   to: Routes.user_auth_login_path(@socket, :new),
                   class:
-                    "#{if(@active_path == "/auth/login", do: "bg-indigo-500 text-gray-50 border-transparent hover:bg-indigo-600 hover:border-indigo-600 shadow", else: "bg-transparent text-indigo-500 dark:text-slate-100 hover:bg-indigo-500 hover:border-indigo-500 hover:text-indigo-50")} py-1 px-3 border rounded-full font-semibold duration-150 ease-in-out"
+                    "#{if(@active_path == "/auth/login", do: "bg-indigo-500 text-gray-50 border-transparent hover:bg-indigo-600 hover:border-indigo-600", else: "bg-transparent text-indigo-500 dark:text-slate-100 hover:bg-indigo-500 hover:border-indigo-500 hover:text-indigo-50")} py-1 px-3 border rounded-full font-semibold duration-150 ease-in-out"
                 ) %>
               </li>
               <li>
                 <%= live_redirect("Sign Up",
                   to: Routes.user_auth_sign_up_path(@socket, :new),
                   class:
-                    "#{if(@active_path == "/auth/signup", do: "bg-indigo-500 text-gray-50 border-transparent hover:bg-indigo-600 hover:border-indigo-600 shadow", else: "bg-transparent text-indigo-500 dark:text-slate-100 hover:bg-indigo-500 hover:border-indigo-500 hover:text-indigo-50")} ml-2 py-1 px-3 border rounded-full font-semibold duration-150 ease-in-out"
+                    "#{if(@active_path == "/auth/signup", do: "bg-indigo-500 text-gray-50 border-transparent hover:bg-indigo-600 hover:border-indigo-600", else: "bg-transparent text-indigo-500 dark:text-slate-100 hover:bg-indigo-500 hover:border-indigo-500 hover:text-indigo-50")} ml-2 py-1 px-3 border rounded-full font-semibold duration-150 ease-in-out"
                 ) %>
               </li>
             <% end %>
