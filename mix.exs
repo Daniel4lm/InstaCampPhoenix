260f291b6b2b237b6407484a3266720182171292
Daniel Molnar <daniel4molnar@gmail.com>
2023-04-17 14:59:03 +0200

    Remove old seed file

--- mix.exs
+++ mix.exs
@@ -98,8 +98,7 @@ defmodule Instacamp.MixProject do
       "ecto.setup": [
         "ecto.create",
         "ecto.migrate",
-        "run priv/repo/seeds.exs",
-        "run priv/repo/seeds.dev.exs"
+        "run priv/repo/seeds.exs"
       ],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       test: ["ecto.create --quiet", "ecto.migrate --quiet", "test"],
